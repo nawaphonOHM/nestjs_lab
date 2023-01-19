45913d4a0e4c6bb93ac0183d00b76cac477b2911
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-19 19:53:58 +0700

    setting database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MainController } from './main/main.controller';
 import { CrudService } from './crud/crud.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseSetting } from './database-setting';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseSetting)],
   controllers: [AppController, MainController],
   providers: [AppService, CrudService],
 })
--- src/database-setting.ts
+++ src/database-setting.ts
@@ -1,12 +1,12 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm/dist/interfaces";
-
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces';
 
 export const databaseSetting: TypeOrmModuleOptions = {
-    type: "postgres", 
-    host: "localhost", 
-    port: 5432, 
-    username: "postgres", 
-    password: "pass1234", 
-    database: "postgres", 
-    synchronize: true
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass1234',
+  database: 'postgres',
+  synchronize: true,
+  schema: 'public',
 };
