f7e5cc1df1a42e5cd5864f3da32a61875b46543c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-19 20:46:55 +0700

    added user module at app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { MainController } from './main/main.controller';
 import { CrudService } from './crud/crud.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseSetting } from './database-setting';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseSetting)],
+  imports: [TypeOrmModule.forRoot(databaseSetting), UserModule],
   controllers: [AppController, MainController],
   providers: [AppService, CrudService],
 })
