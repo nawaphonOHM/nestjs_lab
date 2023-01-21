e6ce603758ffbc4f22a1c22a26b02b1f0a95fe2b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-21 22:23:45 +0700

    remove unused controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MainController } from './main/main.controller';
-import { CrudService } from './crud/crud.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseSetting } from './database-setting';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseSetting), UserModule],
-  controllers: [AppController, MainController],
-  providers: [AppService, CrudService],
+  controllers: [MainController],
+  providers: [],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module, ModuleMetadata } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CrudService } from 'src/crud/crud.service';
 import { MainController } from 'src/main/main.controller';
 import { PhotoEntity } from 'src/photo/photo.entity';
 import { UserEntity } from './user.entity';
@@ -8,7 +7,7 @@ import { UserEntity } from './user.entity';
 const metadata: ModuleMetadata = {
   imports: [TypeOrmModule.forFeature([UserEntity, PhotoEntity])],
   controllers: [MainController],
-  providers: [CrudService],
+  providers: [],
 };
 
 @Module(metadata)
