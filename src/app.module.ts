bdcfd5e34e0fba0a23e14cb6f4d58f57a53b3b8a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-21 23:29:41 +0700

    remove unuse controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseSetting } from './database-setting';
-import { MainController } from './user/main.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseSetting), UserModule],
-  controllers: [MainController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
