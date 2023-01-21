ab286b17626832ed70b0a925237aec55d57bb9b1
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-22 00:29:39 +0700

    implement get data from db

--- src/user/main.controller.ts
+++ src/user/main.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserEntity } from 'src/enties/user.entity';
+import { UserService } from './user.service';
 
 @Controller('/typeorm-test')
 export class MainController {
+  constructor(private readonly userService: UserService) {}
+
   @Get('/create-data')
-  public createData(): string {
-    return 'Hello World';
+  public async createData(): Promise<UserEntity[]> {
+    const returnedData: UserEntity[] = [];
+
+    await this.userService.findAll().then((data: UserEntity[]): void => {
+      returnedData.push(...data);
+    });
+
+    return returnedData;
   }
 }
