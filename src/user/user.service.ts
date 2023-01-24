a0071925ade6dcae6d44ac498aa6749ba4343e81
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-24 11:39:44 +0700

    add controller v2

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { userNewStyle } from 'src/enties/new-style-user.entity';
 import { UserEntity } from 'src/enties/user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepo: Repository<UserEntity>,
+    private readonly dataSource: DataSource,
   ) {}
 
-  public async findAll(): Promise<UserEntity[]> {
+  public async findAllV1(): Promise<UserEntity[]> {
     return this.userRepo.find();
+  }
+
+  public async findAllV2(): Promise<UserEntity[]> {
+    const userRepository: Repository<UserEntity> =
+      this.dataSource.getRepository<UserEntity>(userNewStyle);
+
+    return await userRepository.find();
   }
 }
