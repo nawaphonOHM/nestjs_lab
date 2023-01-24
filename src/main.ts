0298a71519103e89195145b5fa4a388f99fd936e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-01-24 10:26:57 +0700

    enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { INestApplication, VersioningType } from '@nestjs/common';
+import { UriVersioningOptions } from '@nestjs/common/interfaces';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  const versioning: UriVersioningOptions = { type: VersioningType.URI };
+
+  app.enableVersioning(versioning);
   await app.listen(3000);
 }
 bootstrap();
